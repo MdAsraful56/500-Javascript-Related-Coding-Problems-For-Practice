061549924d756c5f9e8b621052c6bd059cb5f226
MdAsraful56 <work.mdasraful56@gmail.com>
2025-04-07 00:02:20 +0600

    Array 10 problems solve

---
+++ 1. Array/1.1 Array/problem1.js
@@ -0,0 +1,6 @@
+// তোর বাসায় একটা ছোট ফ্রিজ আছে, যেখানে ঠিক 5টা ফল রাখতে পারিস। তুই একটা অ্যারে বানিয়ে তাতে আপেল, কলা, কমলা, আম আর পেয়ারা রাখলি। এখন তুই দেখবি ফ্রিজে মোট কতটা ফল আছে।
+const fruits = ["আপেল", "কলা", "কমলা", "আম", "পেয়ারা"];
+console.log("ফ্রিজে মোট ফল:", fruits.length);
+
+const fruits2 = ['apple', 'banana', 'mango', 'orange', 'guava'];
+console.log("Total fruits in fridge:", fruits2.length);
